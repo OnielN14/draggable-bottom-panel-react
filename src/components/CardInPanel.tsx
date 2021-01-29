3714e671a175b23efd5bb6626dd77f75f9148808
Daniyal Ahmad Rizaldhi <onieln14@gmail.com>
2021-01-29 15:31:40 +0700

    Initial commit

---
+++ src/components/CardInPanel.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const CardInPanel: React.FC<CardInPanelDataProp & React.HTMLAttributes<HTMLElement>> = ({ name, age, ...restProps }) => {
+  return (
+    <div {...restProps}>
+      <div>
+        <div>Name</div>
+        <div>{name}</div>
+      </div>
+      <div>
+        <div>Age</div>
+        <div>{age}</div>
+      </div>
+    </div>
+  )
+}
+
+export default CardInPanel
